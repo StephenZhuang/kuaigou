f6621043bea9786063fa2b73a301c5777ef8d518
Stephen <379128008@qq.com>
2015-07-03 16:39:15 +0800

    pod update mj

--- KuaiGou/Pods/Target Support Files/Pods/Pods-environment.h
+++ KuaiGou/Pods/Target Support Files/Pods/Pods-environment.h
@@ -111,14 +111,14 @@
 // MJExtension
 #define COCOAPODS_POD_AVAILABLE_MJExtension
 #define COCOAPODS_VERSION_MAJOR_MJExtension 2
-#define COCOAPODS_VERSION_MINOR_MJExtension 0
-#define COCOAPODS_VERSION_PATCH_MJExtension 2
+#define COCOAPODS_VERSION_MINOR_MJExtension 3
+#define COCOAPODS_VERSION_PATCH_MJExtension 7
 
 // MJRefresh
 #define COCOAPODS_POD_AVAILABLE_MJRefresh
 #define COCOAPODS_VERSION_MAJOR_MJRefresh 1
 #define COCOAPODS_VERSION_MINOR_MJRefresh 4
-#define COCOAPODS_VERSION_PATCH_MJRefresh 6
+#define COCOAPODS_VERSION_PATCH_MJRefresh 7
 
 // ObjectiveRecord
 #define COCOAPODS_POD_AVAILABLE_ObjectiveRecord
