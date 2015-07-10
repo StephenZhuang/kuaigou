27bcc898e471b6532ce6311a8a804ba5bb4bc266
Stephen <379128008@qq.com>
2015-07-10 15:49:27 +0800

    temp

---
+++ KuaiGou/KuaiGou/Controllers/附近/KGGoodsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  KGGoodsDetailViewController.h
+//  KuaiGou
+//
+//  Created by Stephen Zhuang on 15/7/10.
+//  Copyright (c) 2015年 Hatlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGGoodsDetailViewController : UIViewController
+
+@end
